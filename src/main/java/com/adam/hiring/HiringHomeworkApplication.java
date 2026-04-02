a3d0826b9d4aee57acdb4d38cded71a567632377
Adam Major <adam.major314@gmail.com>
2026-04-02 11:57:48 +0200

    Init commit

---
+++ src/main/java/com/adam/hiring/HiringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.adam.hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringHomeworkApplication.class, args);
+	}
+
+}
