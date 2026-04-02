549d7de5b481f05118076cd9f769b3275026f933
Adam Major <adam.major314@gmail.com>
2026-04-02 14:02:37 +0200

    Added DTO and Entity field validations for Account

--- src/main/java/com/adam/hiring/account/Account.java
+++ src/main/java/com/adam/hiring/account/Account.java
@@ -2,7 +2,7 @@ package com.adam.hiring.account;
 
 import com.adam.hiring.shared.enums.Currency;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,11 +20,18 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Account name cannot be blank")
+    @Size(min = 2, max = 100, message = "Account name must be between 2 and 100 characters")
+    @Column(nullable = false, length = 100)
     private String name;
 
+    @NotNull(message = "Balance cannot be null")
+    @Column(nullable = false, precision = 14, scale = 2)
+    //TODO: Negative balance is allowed? If not, we should use @PositiveOrZero annotation too
     private BigDecimal balance;
 
+    @NotNull(message = "Currency cannot be null")
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Currency currency;
-
 }
--- src/main/java/com/adam/hiring/account/AccountDto.java
+++ src/main/java/com/adam/hiring/account/AccountDto.java
@@ -1,12 +1,23 @@
 package com.adam.hiring.account;
 
 import com.adam.hiring.shared.enums.Currency;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record AccountDto(
         Long id,
+
+        @NotBlank(message = "Account name cannot be blank")
         String name,
+
+        @NotNull(message = "Balance cannot be null")
+        @Digits(integer = 12, fraction = 2, message = "Balance must have at most 12 integer digits and 2 fractional digits")
+        //TODO: Negative balance is allowed? If not, we should use @PositiveOrZero annotation too
         BigDecimal balance,
+
+        @NotNull(message = "Currency cannot be null")
         Currency currency
 ) { }
